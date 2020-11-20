febafa0612392261ae12d448ec181dcdca4a038f
npipkins <68622679+npipkins@users.noreply.github.com>
2020-11-19 20:48:56 -0600

    bar chart

--- static/js/app.js
+++ static/js/app.js
@@ -1,3 +1,22 @@
 //Use d3 to pull in the data
-d3.json("http://localhost:8000/samples.json").then(data);
-    console.log(data);
+d3.json("data/samples.json").then((data) => {
+
+//Create the trace
+  var trace1 = {
+    x: data.map(row => samples.sample_values),
+    y: data.map(row => samples.otu_ids),
+    type: "bar",
+  };
+
+//Make the data array
+var data = [trace1];
+
+//Define the layout
+var layout = {
+    title: "Bar Chart"
+};
+
+//Plot the chart
+Plotly.newPlot("bar", data, layout);
+
+});
