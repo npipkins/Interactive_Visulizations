ca250541181ae1546988f12348d3123f263704f1
npipkins <68622679+npipkins@users.noreply.github.com>
2020-11-19 21:04:49 -0600

    bubble chart

--- static/js/app.js
+++ static/js/app.js
@@ -1,7 +1,7 @@
 //Use d3 to pull in the data
 d3.json("data/samples.json").then((data) => {
 
-//Create the trace
+//Create the bar chart trace
   var trace1 = {
     x: data.map(row => samples.sample_values),
     y: data.map(row => samples.otu_ids),
@@ -18,5 +18,23 @@ var layout = {
 
 //Plot the chart
 Plotly.newPlot("bar", data, layout);
+});
 
+//Create the bubble chart trace
+var trace2 = {
+    x: data.map(row => samples.otu_ids),
+    y: data.map(row => samples.samples_values),
+    type: "bubble",
+  };
+
+//Make the data array
+var data = [trace2];
+
+//Define the layout
+var layout = {
+    title: "Bubble Chart"
+};
+
+//Plot the chart
+Plotly.newPlot("bubble", data, layout);
 });
